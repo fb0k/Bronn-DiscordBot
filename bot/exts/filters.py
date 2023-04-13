4de7f72d879b214669aa394e0dae8d83028cf13c
Fabio Sousa <soufabio.df@gmail.com>
2023-04-13 20:05:36 -0300

    Removed boilerplate code, changed the way list_all_data receive the cached values from the filterlist cache and removed the comment bit of the code since currently theres no comments being passed from the whitelist and blacklist methods.

--- bot/exts/filters.py
+++ bot/exts/filters.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from tortoise.exceptions import OperationalError
-from database.models import Filterlist
+from database.models import Filterlist, Guild
+from tortoise.functions import Concat, Coalesce
+from tortoise.expressions import F
 import discord
 from discord.ext.commands import BadArgument, Cog, Context, command, has_any_role
 import constants
@@ -8,7 +10,10 @@ from Bronn import Bot
 from constants import Colours
 from log import get_logger
 from utils.paginator import LinePaginator
-
+from collections import defaultdict
+import Bronn
+import sys
+import asyncio
 
 log = get_logger(__name__)
 
@@ -20,23 +25,13 @@ class Filters(Cog):
         self.bot = bot
         self.synced_guilds: list = []
 
-    async def check_guildcache(self, ctx: Context):
-        if ctx.guild.id not in self.synced_guilds:
-            await self.bot.cache_filter_list_data(ctx)
-            self.synced_guilds.append(ctx.guild.id)
-
-    @command(name="whitelist", aliases=("allowlist", "allow", "al", "wl"))
+    @command(name="whitelist", aliases=("allowlist", "wl"))
     async def whitelist(
         self,
         ctx: Context,
         file_type: str,
-        comment: Optional[str] = None,
-        allow: bool = True,
     ) -> None:
-        """Whitelist a file extension, remove it from blacklist if needed."""
-
-        # check if guild has cached files, otherwise get it from database
-        await self.check_guildcache(ctx)
+        """Add a file type to the Whitelist."""
 
         # assert it has a leading dot.
         if not file_type.startswith("."):
@@ -44,109 +39,79 @@ class Filters(Cog):
 
         # Try to add the item to the database
         log.trace(f"Trying to whitelist the {file_type} file")
-        item = self.bot.filter_list_cache[f"{ctx.guild.id}"].get(file_type)
-
+        # item = self.bot.filter_list_cache[f"{ctx.guild.id}"]
+        guild = ctx.guild.id
         try:
-            # if in cache and not blacklisted, modify it
-            if item is not None and item["allow"] is False:
-                revertbool = await Filterlist.get(type=file_type, guild=ctx.guild.id, allowed=False)
-                await revertbool.revertit(allow)
-            elif item is None:  # if None, create it
-                await Filterlist.create(
-                    type=file_type,
-                    guild_id=ctx.guild.id,
-                    allowed=allow,
-                    comment=comment,
-                )
-            else:  # if already whitelisted, pass
+            cached_item = self.bot.filter_list_cache[f"{ctx.guild.id}"]
+            if file_type in cached_item:
                 pass
+            elif len(cached_item) == 0:  # if None, create it
+                item = await Filterlist.create(guild_id=guild, whitelist=[file_type])
 
-            item = {"type": file_type, "guild_id": ctx.guild.id, "allowed": allow, "comment": comment}
-            self.bot.insert_item_into_filter_list_cache(item)
-            await ctx.message.add_reaction("✅")
-            await ctx.reply(f"File `{file_type}` whitelisted.")
+            else:
+                item = await Filterlist.append_by_guild("whitelist", file_type, guild)
 
-        except Exception:
-            await ctx.message.add_reaction("❌")
-            log.debug(f"{ctx.author} tried to add filetype to whitelist, but tortoise returned a operational error. ")
-            raise BadArgument(f"Unable to add the {file_type} to the whitelist. " "Check args and variables")
+        except KeyError:
+            item = await Filterlist.create(guild_id=guild, whitelist=[file_type])
+            log.error(f"{ctx.author} tried whitelist {file_type}, but tortoise returned a key error. ")
+            # raise BadArgument(f"Unable to add the {file_type} to the whitelist. " "Check args and variables")
+
+        whitelist = item.whitelist
+        log.trace(f"Updating {guild} filterlist cache...")
+        self.bot.insert_item_into_filter_list_cache(guild, whitelist)
+        await ctx.message.add_reaction("✅")
+        await ctx.reply(f"File `{file_type}` whitelisted.")
 
     @command(name="blacklist", aliases=("denylist", "deny", "bl", "dl"))
     async def blacklist(
         self,
         ctx: Context,
         file_type: str,
-        comment: Optional[str] = None,
-        allow: bool = False,
     ) -> None:
-        """Blacklist a file extension, remove it from whitelist if needed."""
-
-        # check if guild has cached files, otherwise get it from database
-        await self.check_guildcache(ctx)
+        """Blacklist a file extension, removing it from whitelist."""
 
         # assert it has a leading dot.
         if not file_type.startswith("."):
             file_type = f".{file_type}"
 
         # Find the file in the cache
-        log.trace(f"Trying to blacklist the {file_type} file")
-        item = self.bot.filter_list_cache[f"{ctx.guild.id}"].get(file_type)
-
+        guild = ctx.guild.id
         try:
-            # if in cache and not blacklisted, modify it
-            if item is not None and item["allow"] is True:
-                revertbool = await Filterlist.get(type=file_type, guild=ctx.guild.id, allowed=True)
-                await revertbool.revertit(allow)
+            log.trace(f"Checking for {file_type} in the filterlist chache")
+            cached_item = self.bot.filter_list_cache[f"{ctx.guild.id}"]
 
-            elif item is None:  # if None, create it
-                await Filterlist.create(
-                    type=file_type,
-                    guild_id=ctx.guild.id,
-                    allowed=allow,
-                    comment=comment,
-                )
+            if file_type in cached_item:
+                item = await Filterlist.remove_by_guild("whitelist", file_type, guild)
             else:  # if already blacklisted, pass
                 pass
+        except KeyError:
+            item = await Filterlist.create(guild_id=guild, whitelist=[])
+        except Exception as e:
+            log.error(e)
 
-            # create dict from passes args, and store it in the cachelist
-            item = {"type": file_type, "guild_id": ctx.guild.id, "allowed": allow, "comment": comment}
-            self.bot.insert_item_into_filter_list_cache(item)
-            await ctx.message.add_reaction("✅")
-            await ctx.reply(f"File `{file_type}` blacklisted.")
-        except Exception:
-            await ctx.message.add_reaction("❌")
-            log.debug(
-                f"{ctx.author} tried to add filetype to the blacklist, but tortoise returned a operational error. "
-            )
-            raise BadArgument(f"Unable to add the {file_type} to the blacklist. Check args and variables")
+        whitelist = item.whitelist
+        log.trace(f"Updating {guild} filterlist cache...")
+        self.bot.insert_item_into_filter_list_cache(guild_id=guild, whitelist=whitelist)
+        await ctx.message.add_reaction("✅")
+        await ctx.reply(f"File `{file_type}` blacklisted.")
 
-    @command(name="filterlist", aliases=("filelist", "fl", "fetch"))
-    async def _list_all_data(self, ctx: Context, allowed: bool) -> None:
+    @command(name="filterlist")
+    async def _list_all_data(self, ctx: Context) -> None:
         """Paginate and display all items in the filterlist."""
-
-        # check if guild has cached files, otherwise get it from database
-        await self.check_guildcache(ctx)
 
         # Return only files that match the passed 'allowed: bool'
         # .items() to get 'allow' values , since results is a dict
-        result = {k: v for k, v in self.bot.filter_list_cache[f"{ctx.guild.id}"].items() if v["allow"] == allowed}
+        result = [v for v in self.bot.filter_list_cache[f"{ctx.guild.id}"]]
 
         # Build a list of lines we want to show in the paginator
         lines = []
-        for file, details in result.items():
+        for file in result:
             line = f"• `{file}`"
-
-            if comment := details.get("comment"):
-                line += f" - {comment}"
 
             lines.append(line)
         lines = sorted(lines)
 
-        # Build the embed, change title and color based on passed 'allowed: bool'
-        if allowed is False:
-            embed = discord.Embed(title=f"{ctx.guild}'s latest blacklist", colour=Colours.error)
-        else:
-            embed = discord.Embed(title=f"{ctx.guild}'s latest whitelist", colour=Colours.blue)
+        embed = discord.Embed(title=f"{ctx.guild}'s latest whitelist", colour=Colours.blue)
 
         log.trace(f"Trying to list {len(result)} items from the {ctx.guild} Filterlist")
 
